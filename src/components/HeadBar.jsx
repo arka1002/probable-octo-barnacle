a86ee2e95fb0c2104803785ac44a9bac9af05461
arka1002 <arka.mcu700@gmail.com>
2023-04-06 20:55:10 +0530

    save now

--- src/components/HeadBar.jsx
+++ src/components/HeadBar.jsx
@@ -38,12 +38,24 @@ export const options = {
 
 
 const labels = ['2023', '2024', '2025', '2026', '2027'];
+export const data = {
+    labels,
+    datasets: [
+        {
+            fill: true,
+            label: 'Dataset 2',
+            data: [10, 20, 30, 40, 50],
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+    ],
+};
 
 
 export default function HeadBar() {
     return (
         <div>
-
+            <Line options={options} data={data} />
         </div>
     )
 };
