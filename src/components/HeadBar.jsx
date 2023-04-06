06c8bdc439b1589dfc7787f5931f5fbd280c629c
arka1002 <arka.mcu700@gmail.com>
2023-04-06 20:50:42 +0530

    hint

--- src/components/HeadBar.jsx
+++ src/components/HeadBar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Filler,
-  Legend,
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 
@@ -21,13 +21,29 @@ ChartJS.register(
     Tooltip,
     Filler,
     Legend
-  );
+);
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart',
+        },
+    },
+};
+
+
+const labels = ['2023', '2024', '2025', '2026', '2027'];
 
 
 export default function HeadBar() {
     return (
         <div>
-            
+
         </div>
     )
 };
