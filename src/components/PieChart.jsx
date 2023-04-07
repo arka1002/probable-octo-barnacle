80f69a155a1fd78346149f5a287a5b4ec18534b7
arka1002 <arka.mcu700@gmail.com>
2023-04-07 15:08:52 +0530

    save

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -5,28 +5,35 @@ import { Doughnut } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const data = {
-  labels: ['Revenue', 'Cost'],
-  datasets: [
-    {
-      data: [46200, 1424],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)'
-      ],
-      borderWidth: 1,
-    },
-  ],
+    labels: ['Revenue', 'Cost'],
+    datasets: [
+        {
+            data: [46200, 1424],
+            backgroundColor: [
+                '#0e7469',
+                '#cfd8dc'
+            ],
+            borderColor: [
+                '#0e7469',
+                '#cfd8dc'
+            ],
+            borderWidth: 1,
+        },
+    ],
 };
 
 export const options = {
     maintainAspectRatio: false,
+    cutout: '75%',
     plugins: {
         legend: {
-            position: 'right'
+            position: 'right',
+            boxWidth: 10,
+            usePointStyle: true,
+            pointStyle: 'circle',
+            labels: {
+                usePointStyle: true,
+            },
         }
     }
 }
@@ -34,7 +41,12 @@ export const options = {
 export default function PieChart() {
     return (
         <>
-            <Doughnut data={data} />
+            <Doughnut
+                data={data}
+                options={options}
+                height={280}
+                width={385} />
+
         </>
     )
 };
