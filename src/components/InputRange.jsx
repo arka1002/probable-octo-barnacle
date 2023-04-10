bbb6df155cf14931c6511979200d7b2cc61927a4
arka1002 <arka.mcu700@gmail.com>
2023-04-10 15:07:01 +0530

    will put the js now

--- src/components/InputRange.jsx
+++ src/components/InputRange.jsx
@@ -1,12 +1,20 @@
 import styles from './InputRange.module.css'
-
+import { useState } from "react";
 
 export default function InputRange() {
+    const [value, setValue] = useState(25);
+
+
     return (
         <>
-            <div>
+            <div className='pt-16'>
+                <div className='relative'>
+                    <p className={styles.upperp}>{value}</p>
+                    <span className={styles.upperspan}></span>
+                </div>
                 {/* Needed to add CSS Modules because tailwind css doesnt support the necessary pseudo-elements required for styling */}
-                <input type="range" id="test" className={styles.special} min={0} max={100} />
+                <input type="range" id="test" className={styles.special} min={0} max={100} value={value} onChange={e => setValue(e.target.value)} />
+                <p>{value}</p>
             </div>
         </>
     );
