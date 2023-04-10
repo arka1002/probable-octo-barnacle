7d18139d4e930a3259efd80114ed28e9682ab786
arka1002 <arka.mcu700@gmail.com>
2023-04-10 14:06:02 +0530

    point

--- src/components/InputRange.jsx
+++ src/components/InputRange.jsx
@@ -5,8 +5,8 @@ export default function InputRange() {
     return (
         <>
             <div>
+                {/* Needed to add CSS Modules because tailwind css doesnt support the necessary pseudo-elements required for styling */}
                 <input type="range" id="test" className={styles.special} min={0} max={100} />
-                {/* <p className={styles.random}>asdfghjkl</p> */}
             </div>
         </>
     );
