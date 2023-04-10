8c44c534d7cd7c1faefeedd0512f512c3f68a0de
arka1002 <arka.mcu700@gmail.com>
2023-04-10 15:21:51 +0530

    save

--- src/components/InputRange.jsx
+++ src/components/InputRange.jsx
@@ -13,7 +13,18 @@ export default function InputRange() {
                     <span className={styles.upperspan}></span>
                 </div>
                 {/* Needed to add CSS Modules because tailwind css doesnt support the necessary pseudo-elements required for styling */}
-                <input type="range" id="test" className={styles.special} min={0} max={100} value={value} onChange={e => setValue(e.target.value)} />
+                <input
+                    type="range"
+                    id="test"
+                    className={styles.special}
+                    min={0}
+                    max={100}
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                    style={{
+                        backgroundImage: `linear-gradient(to right, #0e7469  0%, #1fc39e ${value}%, #d9d9d9 ${value}%, #d9d9d9 100%)`,
+                    }} 
+                    />
                 <p>{value}</p>
             </div>
         </>
