bd2935c5ac526c51e50670f0f5fcbbb00a3fbf3e
arka1002 <arka.mcu700@gmail.com>
2023-04-10 21:35:27 +0530

    save

--- src/components/Valuation/ScoreCard.jsx
+++ src/components/Valuation/ScoreCard.jsx
@@ -1,4 +1,5 @@
 import { ReactComponent as Plus } from "../../assets/plus.svg";
+import { ReactComponent as Pen } from "../../assets/pen.svg";
 
 
 
@@ -11,6 +12,20 @@ export default function SC() {
             "Factor"
         ],
     }
+    const items = [
+        {
+            valueDriver: "Strength of the Management Team",
+            weight: "30%",
+            yvs: "100%",
+            factor: "0.3"
+        },
+        {
+            valueDriver: "Size of the Opportunity",
+            weight: "15%",
+            yvs: "100%",
+            factor: "0.15"
+        }
+    ]
     return (
         <>
             <div className="p-7 bg-white">
@@ -27,6 +42,26 @@ export default function SC() {
                             {data.headers.map(header => <th scope="col" className="text-center py-4 text-[#1FC39E] font-bold text-sm">{header}</th>)}
                         </tr>
                     </thead>
+                    <tbody>
+                        {items.map(item => (
+                            <>
+                                <tr>
+                                    <td className="grid place-items-center py-4"><Pen /></td>
+                                    <td className="text-center py-4 px-1 lg:px-3 font-normal text-sm text-gray-600">{item.valueDriver}</td>
+                                    <td className="text-center py-4 px-1 lg:px-3 font-normal text-sm text-gray-600">{item.weight}</td>
+                                    <td className="text-center py-4 px-1 lg:px-3 font-normal text-sm text-gray-600">{item.yvs}</td>
+                                    <td className="text-center py-4 px-1 lg:px-3 font-normal text-sm text-gray-600">{item.factor}</td>
+                                </tr>
+                            </>
+                        ))}
+                        <tr>
+                            <td></td>
+                            <td className="py-4 px-1 lg:px-3 text-center font-bold text-sm text-gray-600">Factor Sum</td>
+                            <td className="py-4 px-1 lg:px-3 text-center font-bold text-sm text-gray-600">100%</td>
+                            <td></td>
+                            <td className="text-center py-4 px-1 lg:px-3 font-normal text-sm text-gray-600">1</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </>
