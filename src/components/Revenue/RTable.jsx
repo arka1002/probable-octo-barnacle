7607a81a3c7442176e04b5c6d6ad9e688e7f715f
arka1002 <arka.mcu700@gmail.com>
2023-04-12 16:23:40 +0530

    test

--- src/components/Revenue/RTable.jsx
+++ src/components/Revenue/RTable.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 
 const RTable = () => {
   const [data, setData] = useState();
-  const [toggle, setToggle] = useState(false);
-
+  // const [toggle, setToggle] = useState(false);
+  const [time, setTime] = useState('year')
   const yearList = ["2023", "2024", "2025", "2026", "2027", "2028", "2029"];
   const [yr, setYr] = useState(yearList);
 
@@ -12,169 +12,69 @@ const RTable = () => {
       id: 1,
       yr: "2023",
       val: "04501",
-      month: [
-        "2023",
-        "jan",
-        "feb",
-        "mar",
-        "aprl",
-        "may",
-        "june",
-        "july",
-        "aug",
-      ],
-      data: ["1", "2", "3", "4", "5", "6", "7", "8"],
+      month: ["2023","jan", "feb", "mar", "aprl", "may", "june", "july", "aug"],
+      score: ["1", "2", "3", "4", "5", "6", "7", "8"],
+      test: ["1", "2", "3", "4", "5", "6", "7", "8"],
     },
     {
       id: 2,
       yr: "2024",
       val: "04502",
-      month: ["jan", "feb", "mar", "aprl", "may", "june", "july", "aug"],
-      data: ["1", "2", "3", "4", "5", "6", "7", "8"],
+      month: ["2024","jan", "feb", "mar", "aprl", "may", "june", "july", "aug"],
+      score: ["1", "2", "3", "4", "5", "6", "7", "8"],
+      test: ["1", "2", "3", "4", "5", "6", "7", "8"],
     },
     {
       id: 3,
       yr: "2025",
       val: "04501",
-      month: ["jan", "feb", "mar", "aprl", "may", "june", "july", "aug"],
-      data: ["1", "2", "3", "4", "5", "6", "7", "8"],
+      month: ["2025","jan", "feb", "mar", "aprl", "may", "june", "july", "aug"],
+      score: ["1", "2", "3", "4", "5", "6", "7", "8"],
+      test: ["1", "2", "3", "4", "5", "6", "7", "8"],
     },
     {
       id: 4,
       yr: "2026",
       val: "04501",
-      month: ["jan", "feb", "mar", "aprl", "may", "june", "july", "aug"],
-      data: ["1", "2", "3", "4", "5", "6", "7", "8"],
+      month: ["2026","jan", "feb", "mar", "aprl", "may", "june", "july", "aug"],
+      score: ["1", "2", "3", "4", "5", "6", "7", "8"],
+      test: ["1", "2", "3", "4", "5", "6", "7", "8"],
     },
     {
       id: 5,
       yr: "2027",
       val: "04501",
-      month: ["jan", "feb", "mar", "aprl", "may", "june", "july", "aug"],
-      data: ["1", "2", "3", "4", "5", "6", "7", "8"],
-    },
-    {
-      id: 6,
-      yr: "2028",
-      val: "04501",
-      month: ["jan", "feb", "mar", "aprl", "may", "june", "july", "aug"],
-      data: ["1", "2", "3", "4", "5", "6", "7", "8"],
-    },
-    {
-      id: 7,
-      yr: "2029",
-      val: "04501",
-      month: ["jan", "feb", "mar", "aprl", "may", "june", "july", "aug"],
-      data: ["1", "2", "3", "4", "5", "6", "7", "8"],
-    },
-    {
-      id: 8,
-      yr: "2029",
-      val: "04501",
-      month: ["jan", "feb", "mar", "aprl", "may", "june", "july", "aug"],
-      data: ["1", "2", "3", "4", "5", "6", "7", "8"],
-    },
-    {
-      id: 9,
-      yr: "2029",
-      val: "04501",
-      month: ["jan", "feb", "mar", "aprl", "may", "june", "july", "aug"],
-      data: ["1", "2", "3", "4", "5", "6", "7", "8"],
+      month: ["2027","jan", "feb", "mar", "aprl", "may", "june", "july", "aug"],
+      score: ["1", "2", "3", "4", "5", "6", "7", "8"],
+      test: ["1", "2", "3", "4", "5", "6", "7", "8"],
     },
   ];
   const [details, setDetails] = useState(values);
-
-  function Get(id) {
-    console.log(id);
-    setData(
-      [details.find((e) => e.id === id)].map((val) => {
-        console.log(val);
-        setYr(val.month);
-        setDetails(val.data);
-      })
-    );
-    setToggle(!toggle);
-    if (!toggle) {
-      setYr(yearList);
-      setDetails(values);
+  function Change(id) {
+    if (time === 'year') {
+      setTime('month')
+      const found = values.find(item => item.id === id)
+      console.log(values.map(x => x.yr));
+    } else {
+      setTime('year')
     }
   }
-
   return (
     <div className="container shadow-md bg-white rounded-lg pb-4 ">
       <div className="overflow-x-auto">
-        <table className="w-full ">
-          <thead className="border-b  border-customGreen-200">
-            <tr>
-              <th className="w-32 px-1 py-4 text-sm font-bold lg:px-3 text-customGreen-200 capitalized">
-                {"Source"}
-              </th>
-              {yr.map((item, i) => (
-                <TableHeadingItem centerTitle={true} title={item} key={i} />
-              ))}
-            </tr>
-          </thead>
-          {/* <tbody>
-                        <tr className='border-b'>
-                            <td className='px-2 py-4 text-sm  font-semibold text-center text-customBlue-darkest'>Apple</td>
-                            {Values.map((item, i) => (
-                                 <TableDataItem value={item} key={i}/>
-                            ))}
-                        </tr>
-                 
-
-                </tbody> */}
-          <tbody>
-            <tr className="border-b">
-              <td className="px-2 py-4 text-sm  font-semibold text-center text-customBlue-darkest">
-                Apple
-              </td>
-              {/* {details.map((item, i) => (
-                    <TableDataItem
-                    
-                      value={item.val}
-                      key={i}
-                      onclick={() => Get(item.id)}
-                    />
-                  ))} */}
-              {details.map((item, id) => (
-                <td
-                  key={id}
-                  onClick={() => Get(item.id)}
-                  className="px-1 py-4 text-sm font-semibold text-center lg:px-3 text-customBlue-100 hover:bg-customGreen-200 hover:text-white"
-                >
-                  {item.val}
-                </td>
-              ))}
-            </tr>
-          </tbody>
-        </table>
+        
       </div>
     </div>
   );
 };
 
 export default RTable;
-const TableHeadingItem = ({ title, centerTitle = true }) => {
-  return (
-    <th
-      className={
-        "py-4 px-1 lg:px-3 font-bold text-sm text-customGreen-200 capitalized" +
-        (centerTitle ? " text-center" : " text-left")
-      }
-    >
-      {title}
-    </th>
-  );
-};
 
-const TableDataItem = ({ value, onclick }) => {
-  return (
-    <td
-      onClick={onclick}
-      className="px-1 py-4 text-sm font-semibold text-center lg:px-3 text-customBlue-100 hover:bg-customGreen-200 hover:text-white"
-    >
-      {value}
-    </td>
-  );
-};
+
+
+
+
+
+
+
+
